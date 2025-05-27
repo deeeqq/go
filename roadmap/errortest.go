dfc0db1bd986b56f582e24500b8a1cacb5222242
deeeqq <neonxxleon@yandex.ru>
2025-05-27 20:18:58 +0300

    errortest.go changed

--- roadmap/errortest.go
+++ roadmap/errortest.go
@@ -5,6 +5,28 @@ import (
 	"fmt"
 )
 
+type CustomError struct {
+	Code int
+	Msg  string
+}
+
+func (e *CustomError) Error() string {
+	return fmt.Sprintf("Ошибка %d: %s", e.Code, e.Msg)
+}
+
+func doSomething() error {
+	return &CustomError{Code: 404, Msg: "Ресурс не найден"}
+}
+func mayPanic() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Поймана паника:", r)
+		}
+	}()
+
+	panic("непредвиденная ошибка!")
+}
+
 // Функция, выполняющая деление и возвращающая ошибку при некорректном вводе
 func divide(a, b float64) (float64, error) {
 	if b == 0 {
@@ -14,6 +36,12 @@ func divide(a, b float64) (float64, error) {
 }
 
 func main() {
+	err := doSomething()
+	if err != nil {
+		fmt.Println("Произошла ошибка:", err)
+	}
+	mayPanic()
+	fmt.Println("Программа продолжает работу")
 	result, err := divide(10, 0)
 	if err != nil {
 		fmt.Println("Ошибка:", err)
