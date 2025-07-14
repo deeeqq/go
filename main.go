1c6bc31a29f3f5d71d611427c011fcd6a63fd4bc
deeeqq <neonxxleon@yandex.ru>
2025-07-14 17:53:13 +0300

    main.go changed

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
 import (
+	"first-step-project/roadmap/stage4"
+	"first-step-project/roadmap/stage5"
 	"first-step-project/roadmap/stage6"
+	"fmt"
 )
 
 func main() {
+	for {
+		var i int
+		fmt.Printf("_______________________________________________________________________________________  \n")
+		fmt.Printf("1)Основы \n")
+		fmt.Printf("2)Ошибки \n")
+		fmt.Printf("3)Структуры \n")
+		fmt.Printf("4)Вызов функции \n")
+		fmt.Printf("5)Рекурсия \n")
+		fmt.Printf("Введите номер пункта: ")
+		fmt.Scanf("%d", &i)
+		switch i {
+		case 1:
+			stage4.Pracmenu()
+		case 2:
+			stage5.Errortest()
+		case 3:
+			stage5.Structuretest()
+		case 4:
+			stage6.Imp3()
+		case 5:
+			stage6.Ot0do5rec()
+		default:
+			fmt.Printf("Такого пункта не существует")
 
-	stage6.Imp3()
+		}
+	}
 }
